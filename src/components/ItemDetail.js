38bb483b9e75ca21bd4209675dacffd645919316
scacosso <sergiocacosso@gmail.com>
2021-06-15 20:24:16 -0300

    arreglo data null

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -13,23 +13,26 @@ function ItemDetail() {
   }, []);
   return (
     <>
-                <div class="card m-5 p-5" >
-            <div class="row no-gutters">
-                <div class="col-sm-5" >
-                    <img src={data.thumbnail} class="card-img-top" alt="producto"></img>
+        {data !== null ? (
+        <div className="card m-5 p-5" >
+            <div className="row no-gutters">
+                <div className="col-sm-5" >
+                    <img src={data.thumbnail} className="card-img-top" alt="producto"></img>
                 </div>
-                <div class="col-sm-7">
+                <div className="col-sm-7">
                 <h4 className="justify-content-center col-10 m-5 p-3 bg-dark rounded-lg text-white shadow-sm">Detalle de Producto</h4>
-                    <div class="card-body">
-                        <h5 class="card-title">{data.title}</h5>
-                        <p class="card-text">{data.tags}</p>
+                    <div className="card-body">
+                        <h5 className="card-title">{data.title}</h5>
+                        <p className="card-text">{data.tags}</p>
                         <h2 className="font-weight-lighter bg-success text-white rounded">${data.price}</h2>
                         <small>Stock: {data.sold_quantity}</small>
                         <ItemCount stock={data.sold_quantity} />
                     </div>
                 </div>
             </div>
-        </div>        
+        </div> ) : (
+        <h3>Cargada</h3>
+      )}      
     </>
   );
 }
